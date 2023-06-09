12b6744580116f6456f376200c7ed202da5e03a2
yifu-deng <iyifu.deng@gmail.com>
2023-06-08 20:51:00 -0400

    update BreakContinue

--- Exercises/BreakContinue.java
+++ Exercises/BreakContinue.java
@@ -4,11 +4,11 @@
 public class BreakContinue {
     public static void windowPosSum(int[] a, int n) {
         /** your code here */
-        for (int i = 0; i < a.length; i++) {
+        for (int i = 0; i < a.length; i += 1) {
             if (a[i] < 0) {
                 continue;
             }
-            for (int j = 1; j <= n; j++) {
+            for (int j = 1; j <= n; j += 1) {
                 if (i + j >= a.length) {
                     break;
                 }
@@ -18,7 +18,7 @@ public class BreakContinue {
     }
 
     public static void main(String[] args) {
-        int[] a = {1, 2, -3, 4, 5, 4};
+        int[] a = { 1, 2, -3, 4, 5, 4 };
         int n = 3;
         windowPosSum(a, n);
 
