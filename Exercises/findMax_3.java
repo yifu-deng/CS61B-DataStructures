d09f5308cfa765d9741a6bdef1587264a4eea1f4
yifu-deng <iyifu.deng@gmail.com>
2023-06-08 20:56:40 -0400

    update for loop to enhanced for loop

--- Exercises/findMax_3.java
+++ Exercises/findMax_3.java
@@ -5,15 +5,23 @@ public class findMax_3 {
     /** Returns the maximum value from m using a for loop. */
     public static int forMax(int[] m) {
         int max = 0;
-        for (int i = 0; i < m.length; i += 1) {
-            if (m[i] > max) {
-                max = m[i];
+        for (int i : m) { // enhanced for loop
+            if (i > max) {
+                max = i;
             }
         }
+        /*
+         for (int i = 0; i < m.length; i += 1) {
+         if (m[i] > max) {
+         max = m[i];
+         }
+         }
+        */
         return max;
     }
+
     public static void main(String[] args) {
-        int[] numbers = new int[]{9, 2, 15, 2, 22, 10, 6};
+        int[] numbers = new int[] { 9, 2, 15, 2, 22, 10, 6 };
         System.out.println(forMax(numbers));
     }
 }
