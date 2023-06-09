83d4d0abbdae412e4be5992794be8c05fee4bdb5
yifu-deng <iyifu.deng@gmail.com>
2023-06-08 20:40:30 -0400

    done with exercise 4

---
+++ Exercises/BreakContinue.java
@@ -0,0 +1,22 @@
+/**
+ * @author yd
+ */
+public class BreakContinue {
+    public static void windowPosSum(int[] a, int n) {
+        /** your code here */
+        for (int i = 0; i <= n; i += 1) {
+            while (a[i] > 0) {
+                if
+            }
+        }
+    }
+
+    public static void main(String[] args) {
+        int[] a = {1, 2, -3, 4, 5, 4};
+        int n = 3;
+        windowPosSum(a, n);
+
+        // Should print 4, 8, -3, 13, 9, 4
+        System.out.println(java.util.Arrays.toString(a));
+    }
+}
