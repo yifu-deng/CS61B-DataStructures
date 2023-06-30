2eb2c9b6d682fd63ed7f9cea346acb669f303c7d
yifu-deng <iyifu.deng@gmail.com>
2023-06-30 16:12:10 -0400

    complete lab3

--- lab3/randomizedtest/TestBuggyAList.java
+++ lab3/randomizedtest/TestBuggyAList.java
@@ -9,4 +9,54 @@ import static org.junit.Assert.*;
  */
 public class TestBuggyAList {
   // YOUR TESTS HERE
+    @Test
+    public void testThreeAddThreeRemove() {
+        AListNoResizing<Integer> rightAnswer = new AListNoResizing<>();
+        BuggyAList<Integer> falseAnswer = new BuggyAList<>();
+
+        for (int i = 4; i < 7; i++) {
+            rightAnswer.addLast(i);
+            falseAnswer.addLast(i);
+        }
+
+        for (int i = 0; i < falseAnswer.size(); i++) {
+            assertEquals(rightAnswer.removeLast(), falseAnswer.removeLast());
+        }
+    }
+
+    @Test
+    public void randomizedTest() {
+        AListNoResizing<Integer> rightAnswer = new AListNoResizing<>();
+        BuggyAList<Integer> falseAnswer = new BuggyAList<>();
+
+        int N = 5000;
+        for (int i = 0; i < N; i += 1) {
+            int operationNumber = StdRandom.uniform(0, 4);
+            if (operationNumber == 0) {
+                // addLast
+                int randVal = StdRandom.uniform(0, 100);
+                rightAnswer.addLast(randVal);
+                falseAnswer.addLast(randVal);
+            } else if (operationNumber == 1) {
+                // size
+                int f_size = falseAnswer.size();
+                int r_size = rightAnswer.size();
+                assertEquals(f_size, r_size);
+            } else  if (operationNumber == 2) {
+                // getLast
+                if (rightAnswer.size() > 0 && falseAnswer.size() > 0) {
+                    int r_num = rightAnswer.getLast();
+                    int f_num = falseAnswer.getLast();
+                    assertEquals(f_num, r_num);
+                }
+            } else if (operationNumber == 3) {
+                // removeLast
+                if (rightAnswer.size() > 0 && falseAnswer.size() > 0) {
+                    int r_num = rightAnswer.removeLast();
+                    int f_num = falseAnswer.removeLast();
+                    assertEquals(f_num, r_num);
+                }
+            }
+        }
+    }
 }
--- lab3/timingtest/TimeSLList.java
+++ lab3/timingtest/TimeSLList.java
@@ -1,4 +1,5 @@
 package timingtest;
+import edu.princeton.cs.algs4.In;
 import edu.princeton.cs.algs4.Stopwatch;
 
 /**
@@ -23,6 +24,30 @@ public class TimeSLList {
 
     public static void timeGetLast() {
         // TODO: YOUR CODE HERE
+        SLList<Integer> N = new SLList<>();
+        AList<Integer> Ns = new AList<>();
+        AList<Double> times = new AList<>();
+        AList<Integer> opCounts = new AList<>();
+
+        int ops = 10000;
+        int[] testSizes = {1000, 2000, 4000, 8000, 16000, 32000, 64000, 128000};
+
+        for (int size: testSizes) {
+            for (int i = 0; i < size; i ++) {
+                N.addLast(i);
+            }
+            Stopwatch sw = new Stopwatch();
+            for (int j = 0; j < ops; j++) {
+                N.getLast();
+            }
+
+            double time = sw.elapsedTime();
+
+            Ns.addLast(size);
+            times.addLast(time);
+            opCounts.addLast(ops);
+        }
+        printTimingTable(Ns, times, opCounts);
     }
 
 }
