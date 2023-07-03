efc86cb1d2020c0b036097b107bacb0293f0cc05
yifu-deng <iyifu.deng@gmail.com>
2023-07-02 20:23:12 -0400

    revise MaxArrayDeque

--- proj1/deque/MaxArrayDeque.java
+++ proj1/deque/MaxArrayDeque.java
@@ -1,4 +1,21 @@
 package deque;
 
-public class MaxArrayDeque {
+import java.util.Comparator;
+
+public class MaxArrayDeque<T> extends ArrayDeque<T>{
+
+    private Comparator<T> comparator;
+
+    public MaxArrayDeque(Comparator<T> c){
+        comparator = c;
+    }
+//  TODO
+    public T max(){
+
+        return null;
+    }
+//    TODO
+    public T max(Comparator<T> c) {
+
+    }
 }
