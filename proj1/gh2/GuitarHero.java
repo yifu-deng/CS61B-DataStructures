c45c7bf5cbdd26b13bc862043053497e2b8d337c
yifu-deng <iyifu.deng@gmail.com>
2023-07-10 08:35:56 -0400

    revise Deque interface

---
+++ proj1/gh2/GuitarHero.java
@@ -0,0 +1,10 @@
+package gh2;
+import edu.princeton.cs.algs4.StdAudio;
+import edu.princeton.cs.algs4.StdDraw;
+
+public class GuitarHero {
+    public static final String KEYBOARD = "q2we4r5ty7u8i9op-[=zxdcfvgbnjmk,.;/' ";
+    public static void main(String[] args) {
+
+    }
+}
